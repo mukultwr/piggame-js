4d61e8a3102156498e24ff2d92bad912cc9c22b9
mukultwr <61946154+mukultwr@users.noreply.github.com>
2022-01-29 14:57:24 +0530

    final project

--- script.js
+++ script.js
@@ -8,24 +8,77 @@ const dice=document.querySelector('.dice');
 const buttonN=document.querySelector('.btn--new');
 const buttonR=document.querySelector('.btn--roll');
 const buttonH=document.querySelector('.btn--hold');
-
+const player0A=document.querySelector('.player--0');
+const player1A=document.querySelector('.player--1');
+const score=[0,0];
 let curScore=0;
-
+let activePlayer=0;
+let playing=true;
 
 score0.textContent=0;
 score1.textContent=0;
 dice.classList.add('hidden');
+const switchPlayer= function(){
+    document.getElementById(`current--${activePlayer}`).textContent=0;
+    activePlayer=activePlayer=== 0?1:0;
+    curScore=0;
+    player0A.classList.toggle('player--active');
+    player1A.classList.toggle('player--active');
+    }
 
-
+//rolling button
 buttonR.addEventListener('click',function(){
+    if(playing){
 //generating a random dice roll so we will use Math.random for generating and Math.trunc to reduce the decimal part
 const diceG=Math.trunc(Math.random()*6)+1;
 //we will display the dice 
 dice.classList.remove('hidden');
-dice.src=`diceG-$(diceG).png`;
+dice.src=`dice-${diceG}.png`;
+
+//checking the roll 1
 if(diceG!==1){
     curScore+=diceG;
-    curP0.textContent=curScore;
+    document.getElementById(`current--${activePlayer}`).textContent=curScore;
+   // curP0.textContent=curScore;
+}
+else{
+    //switch to next player
+   switchPlayer();
+}
+    }
+});
+//hold button
+buttonH.addEventListener('click',function(){
+    if(playing){
+score[activePlayer]+=curScore;
+document.getElementById(`score--${activePlayer}`).textContent=score[activePlayer];
+if(score[activePlayer]>=10){
+    playing =false;
+    dice.classList.add('hidden');
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
 }
 
+else{
+switchPlayer();
+}
+    }
+})
+buttonN.addEventListener('click',function(){
+    //we can make a function of these and can call here only
+    const score=[0,0];
+    let curScore=0;
+    let activePlayer=0;
+    let playing=true;
+    score0.textContent=0;
+    score1.textContent=0
+    curP0.textContent=0;
+    curP1.textContent=0;
+    player0A.classList.remove('player--winner');
+    player1A.classList.remove('player--winner');
+    player0A.classList.add('player--active');
+    player1A.classList.remove('player--active');
+    dice.classList.add('hidden');
+  
 })
